da8ffa5c7cdec6cd44b576403097b7079244a476
jcyuan <1571995@qq.com>
2026-01-13 17:13:43 +0800

    tidy

--- lib/models/sign_in_options.dart
+++ lib/models/sign_in_options.dart
@@ -1,82 +1,54 @@
-/// Email sign in options (parameters correspond to NextAuth callback query parameters)
 class EmailSignInOptions {
   final String email;
   final String code;
-  final String turnstileToken;
+  final String? turnstileToken;
 
-  EmailSignInOptions({required this.email, required this.code, required this.turnstileToken});
+  EmailSignInOptions({required this.email, required this.code, this.turnstileToken});
 
-  Map<String, String> toJson() => {
-    'email': email,
-    'code': code,
-    'turnstile': turnstileToken,
-  };
+  Map<String, String> toJson() {
+    final params = {
+      'email': email,
+      'code': code,
+    };
+    if (turnstileToken != null) {
+      params['turnstile'] = turnstileToken!;
+    }
+    return params;
+  }
 }
 
 class CredentialsSignInOptions {
   final String email;
   final String password;
-  final String turnstileToken;
+  final String? turnstileToken;
 
-  CredentialsSignInOptions({required this.email, required this.password, required this.turnstileToken});
+  CredentialsSignInOptions({required this.email, required this.password, this.turnstileToken});
 
-  Map<String, String> toJson() => {
-    'email': email,
-    'password': password,
-    'turnstile': turnstileToken,
-  };
+  Map<String, String> toJson() {
+    final params = {
+      'email': email,
+      'password': password,
+    };
+    if (turnstileToken != null) {
+      params['turnstile'] = turnstileToken!;
+    }
+    return params;
+  }
 }
 
 class OAuthSignInOptions {
   final String provider;
-  final String turnstileToken;
+  final String? turnstileToken;
 
-  OAuthSignInOptions({required this.provider, required this.turnstileToken});
+  OAuthSignInOptions({required this.provider, this.turnstileToken});
 
-  Map<String, String?> toJson() => {
-    'provider': provider,
-    'turnstile': turnstileToken,
-  };
-}
-
-class RegisterWithCredentialsOptions {
-  final String email;
-  final String password;
-  final String turnstileToken;
-
-  RegisterWithCredentialsOptions({required this.email, required this.password, required this.turnstileToken});
-
-  Map<String, String> toJson() => {
-    'email': email,
-    'password': password,
-    'turnstile': turnstileToken,
-  };
-}
-
-class ForgotPasswordOptions {
-  final String email;
-  final String turnstileToken;
-
-  ForgotPasswordOptions({required this.email, required this.turnstileToken});
-
-  Map<String, String> toJson() => {
-    'email': email,
-    'turnstile': turnstileToken,
-  };
-}
-
-class ResetPasswordOptions {
-  final String email;
-  final String code;
-  final String password;
-  final String turnstileToken;
-
-  ResetPasswordOptions({required this.email, required this.code, required this.password, required this.turnstileToken});
-
-  Map<String, String> toJson() => {
-    'email': email,
-    'code': code,
-    'password': password,
-    'turnstile': turnstileToken,
-  };
+  Map<String, String> toJson() {
+    final params = {
+      'provider': provider,
+    };
+    if (turnstileToken != null) {
+      params['turnstile'] = turnstileToken!;
+    }
+    return params;
+  }
 }
